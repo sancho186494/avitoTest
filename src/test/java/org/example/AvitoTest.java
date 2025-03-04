d845eb9160972d189e2ed60045c39b511f2070ce
alexanderart <test@test.ru>
2025-03-04 15:05:09 +0800

    pom edit

--- src/test/java/org/example/AvitoTest.java
+++ src/test/java/org/example/AvitoTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Test1 {
+public class AvitoTest {
 
     private AvitoHomePage avitoHomePage;
     private AvitoElectronicPage avitoElectronicPage;
